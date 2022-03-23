d616556e3aaf319d956c1dbcaba31c13e3c6c995
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-03-23 10:41:21 +0100

    practice 1 + create Car class

--- src/main/java/se/lexicon/Car.java
+++ src/main/java/se/lexicon/Car.java
@@ -2,9 +2,22 @@ package se.lexicon;
 
 public class Car {
 
-  String color;
-  int numberOfSeats;
-  String model;
   String brand;
+  String registrationNumber;
+  short maxSpeed;
+  String ownerName;
+  String color;
+
+
+  public static void main(String[] args) {
+    Car volvoXC40 = new Car();
+    volvoXC40.color = "gray";
+    volvoXC40.registrationNumber = "abcd-123456";
+    volvoXC40.brand = "Volvo";
+    volvoXC40.ownerName = " VOLVO";
+    volvoXC40.maxSpeed = 200;
+  }
+
+
 
 }
---
+++ src/main/java/se/lexicon/Practice1.java
@@ -0,0 +1,24 @@
+package se.lexicon;
+
+public class Practice1 {
+
+  public static void main (String[] args){
+  /*
+    String firstname = "Mehrdad";
+    System.out.println(firstname);
+  */
+
+  /*
+    String firstName = "Mehrdad";
+    String lastName = "Javan";
+    System.out.println(firstName + " " + lastName);
+  */
+
+    long number1 = 45000L;
+    long number2 = 40000L;
+    long sum = number1 + number2;
+    System.out.println(sum);
+  }
+
+
+}
